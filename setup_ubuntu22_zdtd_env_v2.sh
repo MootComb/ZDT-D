97137eb0374dc4e51cef3f3fc62cb8a2f6fa0f61
MootComb <76607017+MootComb@users.noreply.github.com>
2026-06-07 15:29:17 +0500

    Update setup_ubuntu22_zdtd_env_v2.sh

--- setup_ubuntu22_zdtd_env_v2.sh
+++ setup_ubuntu22_zdtd_env_v2.sh
@@ -221,6 +221,31 @@ export BUILD_TYPE="$BUILD_TYPE"
 export CARGO_PROFILE="$CARGO_PROFILE"
 export CARGO_BUILD_TARGET="$CARGO_BUILD_TARGET"
 export NO_DASHBOARD="1"
+
+export NDK_ROOT="/opt/android-ndk"
+export ANDROID_API=24
+export TARGET_TRIPLE="armv7-linux-androideabi"
+
+# Компиляторы
+export CC="armv7a-linux-androideabi${ANDROID_API}-clang"
+export CXX="armv7a-linux-androideabi${ANDROID_API}-clang++"
+export AR="llvm-ar"
+export LD="${CC}"
+
+# Пути к sysroot и библиотекам
+export SYSROOT="$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
+export LIB_DIR="$SYSROOT/usr/lib/arm-linux-androideabi/${ANDROID_API}"
+
+# Флаги для компиляции и линковки
+export CFLAGS="--sysroot=$SYSROOT -D__ANDROID_API__=$ANDROID_API"
+export CXXFLAGS="--sysroot=$SYSROOT -D__ANDROID_API__=$ANDROID_API"
+export LDFLAGS="-L$LIB_DIR -llog"
+
+# Флаги для Rust
+export RUSTFLAGS="-C linker=${CC} -C link-arg=-llog -C link-arg=-L${LIB_DIR} -C link-arg=--sysroot=${SYSROOT}"
+
+# Добавить NDK в PATH
+export PATH="$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
 ENV
 
   log "Записаны application/local.properties, application/keystore.properties и $ENV_FILE"
